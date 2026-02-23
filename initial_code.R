88d2c18775d5d31d81b0e89c5ec8795a1b0ac85b
lboro-rdm <rdm@lboro.ac.uk>
2026-02-23 14:18:56 +0000

    initial logic

---
+++ initial_code.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(janitor)
+
+
+all_data <- read.csv("batch.csv")
+
+all_data <- all_data %>% 
+  clean_names() %>% 
+  mutate(embargo_date = as.Date(embargo_date))
+
+embargoed_data <- all_data %>%
+  filter(
+    is_embargoed == 1,
+    is.na(embargo_date) | embargo_date > today(),
+    item_type %in% c(
+      "chapter",
+      "journal contribution",
+      "conference contribution"
+    )
+  ) %>% 
+  arrange(embargo_date)
+
+filtered_data <- embargoed_data %>% 
+  select(article_id, title, authors, item_type, handle, embargo_reason, embargo_date, acceptance_date, publication_date, version) %>% 
+  mutate(
+    months_to_embargo = case_when(
+      !is.na(embargo_date) ~ interval(today(), embargo_date) %/% months(1),
+      TRUE ~ NA_integer_
+    )
+  )
+
+filename <- paste0("REFReport_", today(), ".csv")
+
+write.csv(filtered_data, filename, row.names = FALSE)
