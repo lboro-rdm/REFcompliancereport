91363fc9b6a81769daeb9d443c99754ae74dc257
lboro-rdm <rdm@lboro.ac.uk>
2026-03-16 10:56:08 +0000

    clean dates updated

--- clean_dates.R
+++ clean_dates.R
@@ -6,8 +6,12 @@ batch <- read.csv("batch_20260309.csv")
 batch <- batch %>% 
   clean_names()
 
+# publicationdate is the custom field, publication_date is the timeline field
 batch_filtered <- batch %>%
   filter(publicationdate != "" & publication_date == "")
 
-batch_filtered_itemtype <- batch_filtered %>% 
-  filter(item_type == "journal contribution" | item_type == "conference contribution")
+batch_filtered_selected <- batch_filtered %>% 
+  filter(item_type == "journal contribution" | item_type == "conference contribution") %>% select(article_id, title, item_type, handle, publication_date, publicationdate)
+
+write.csv(batch_filtered_selected, "CleanDates-JournalConf.csv")
+
